c33dcb15c341a8f1753efef862a6f6e6dcf32fe7
parkcheayeon <ddaany927@gmail.com>
2025-04-20 21:59:41 +0900

    wateringIntervalText 표시되도록 추가

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,10 +16,12 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,6 +29,9 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
@@ -35,6 +40,7 @@ import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 @Composable
 fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
     val plant by plantDetailViewModel.plant.observeAsState()
+    // LiveData<Plant>을 Compose에서 자동으로 관찰
 
     plant?.let {
         PlantDetailContent(it)
@@ -43,7 +49,13 @@ fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
 
 @Composable
 fun PlantDetailContent(plant: Plant) {
-    PlantName(plant.name)
+    Surface {
+        // 세로 방향으로 아이템 배치
+        Column(Modifier.padding(dimensionResource(R.dimen.margin_normal))) {
+            PlantName(plant.name)
+            PlantWatering(plant.wateringInterval)
+        }
+    }
 }
 
 @Composable
@@ -55,7 +67,39 @@ private fun PlantName(name: String) {
             .fillMaxWidth()
             .padding(horizontal = dimensionResource(R.dimen.margin_small))
             .wrapContentWidth(Alignment.CenterHorizontally)
+        // 가로 너비 max, 좌우 padding, 가운데 정렬
     )
+}
+
+@Composable
+private fun PlantWatering(wateringInterval : Int) {
+    Column(Modifier.fillMaxWidth()) {
+
+        // 좌우 padding, 가운데 정렬
+        val centerWithPaddingModifier = Modifier
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+
+        // 위아래 padding
+        val normalPadding = dimensionResource(R.dimen.margin_normal)
+
+        Text(
+            text = stringResource(R.string.watering_needs_prefix), // watering_needs :
+            color = MaterialTheme.colorScheme.primaryContainer,
+            fontWeight = FontWeight.Bold,
+            modifier = centerWithPaddingModifier.padding(top = normalPadding)
+        )
+
+        // 단수/복수 표현을 위해 pluralStringResource 사용
+        val wateringIntervalText = pluralStringResource(
+            R.plurals.watering_needs_suffix, wateringInterval, wateringInterval
+        )
+
+        Text(
+            text = wateringIntervalText,
+            modifier = centerWithPaddingModifier.padding(bottom = normalPadding)
+        )
+    }
 }
 
 @Preview
@@ -64,5 +108,13 @@ private fun PlantDetailContentPreview() {
     val plant = Plant("id", "Apple", "description", 3, 30, "")
     MaterialTheme {
         PlantDetailContent(plant)
+    }
+}
+
+@Preview
+@Composable
+private fun PlantWateringPreview() {
+    MaterialTheme {
+        PlantWatering(7)
     }
 }
