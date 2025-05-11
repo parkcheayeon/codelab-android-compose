18362c91b77d967af6d54b02c4e8113924858951
parkcheayeon <ddaany927@gmail.com>
2025-05-11 19:55:39 +0900

    AlignYourBodyElement 추가

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,9 +21,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -32,7 +36,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -79,7 +87,20 @@ fun SearchBar(
 fun AlignYourBodyElement(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally, // 가로 방향 중앙 정렬
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ab1_inversions),
+            contentDescription = null,
+            contentScale = ContentScale.Crop, // 이미지가 꽉 차게 자르기
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape) // 컴포저블의 모양 조정 (원형)
+        )
+        Text(text = stringResource(R.string.ab1_inversions))
+    }
 }
 
 // Step: Favorite collection card - Material Surface
