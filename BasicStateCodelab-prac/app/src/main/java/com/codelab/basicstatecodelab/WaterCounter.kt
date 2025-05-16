d1cc9c9bda6908c4d1943260e7b98aba2539a275
parkcheayeon <ddaany927@gmail.com>
2025-05-16 22:36:32 +0900

    상태에 따라 UI 표시

--- BasicStateCodelab-prac/app/src/main/java/com/codelab/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab-prac/app/src/main/java/com/codelab/basicstatecodelab/WaterCounter.kt
@@ -16,10 +16,10 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        // by 키워드를 사용하여 count를 var로 정의할 수 있습니다. 위임의 getter 및 setter 가져오기를 추가하면
-        // 매번 MutableState의 value 속성을 명시적으로 참조하지 않고도 count를 간접적으로 읽고 변경할 수 있습니다.
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
