0b64ed642245317d1529046ac2ead23e0708768c
parkcheayeon <ddaany927@gmail.com>
2025-05-17 11:18:28 +0900

    상태 호이스팅

--- BasicStateCodelab-prac/app/src/main/java/com/codelab/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab-prac/app/src/main/java/com/codelab/basicstatecodelab/WaterCounter.kt
@@ -8,25 +8,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-        if (count > 0) {
-            Text("You've had $count glasses.")
-        }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Add one")
-        }
-    }
-}
-
-// Stateless 컴포저블은 상태를 소유하지 않는 컴포저블입니다. 즉, 새 상태를 보유하거나 정의하거나 수정하지 않습니다.
-// count를 표시하고 count를 늘릴 때 함수를 호출
 @Composable
 fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
@@ -39,10 +25,35 @@ fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = M
     }
 }
 
-// Stateful 컴포저블은 시간이 지남에 따라 변할 수 있는 상태를 소유하는 컴포저블입니다.
-// count 상태를 보유하고 StatelessCounter 함수를 호출할 때 이 상태를 수정
 @Composable
 fun StatefulCounter(modifier: Modifier = Modifier) {
     var count by rememberSaveable { mutableStateOf(0) }
     StatelessCounter(count, { count++ }, modifier)
 }
+
+// 1. 여러 컴포저블이 값을 쓴다면 공통 부모에
+// 2. 값을 바꾸는 주체가 있다면 그 위에
+// 3. 같이 바뀌는 값은 같이 관리
+@Composable
+fun StatefulCounter2() {
+    var waterCount by remember { mutableStateOf(0) }
+    var juiceCount by remember { mutableStateOf(0) }
+
+    StatelessCounter(waterCount, { waterCount++ })
+    StatelessCounter(juiceCount, { juiceCount++ })
+}
+// 사용자가 탭하여 juiceCount를 늘리면 StatefulCounter가 재구성되고
+// juiceCount를 읽는 StatelessCounter도 재구성됩니다.
+// 하지만 waterCount를 읽는 StatelessCounter는 재구성되지 않습니다.
+
+
+@Composable
+fun StatefulCounter3() {
+    var count by remember { mutableStateOf(0) }
+
+    StatelessCounter(count, { count++ })
+    //AnotherStatelessMethod(count, { count *= 2 })
+}
+// 이 경우 개수가 StatelessCounter 또는 AnotherStatelessMethod에 의해 업데이트되면 예상대로 모든 항목이 재구성됩니다.
+// 끌어올린 상태는 공유할 수 있으므로 불필요한 리컴포지션을 방지하고 재사용성을 높이려면 컴포저블에 필요한 상태만 전달해야 합니다.
+// 컴포저블 디자인 권장사항은 필요한 매개변수만 전달하는 것입니다.
