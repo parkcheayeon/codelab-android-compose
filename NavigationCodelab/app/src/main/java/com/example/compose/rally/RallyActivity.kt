687f6f3544dff4c448f034481968e182993c0b8e
parkcheayeon <ddaany927@gmail.com>
2025-06-25 00:45:58 +0900

    RallyNavHost 추가

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -24,17 +24,9 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.compose.rally.ui.accounts.AccountsScreen
-import com.example.compose.rally.ui.accounts.SingleAccountScreen
-import com.example.compose.rally.ui.bills.BillsScreen
 import com.example.compose.rally.ui.components.RallyTabRow
-import com.example.compose.rally.ui.overview.OverviewScreen
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -78,72 +70,10 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            // navController를 전달하여 이 NavHost에 연결
-            NavHost(
+            RallyNavHost(
                 navController = navController,
-                startDestination = Overview.route,
                 modifier = Modifier.padding(innerPadding)
-            ) {
-                // Overview를 composable 확장 함수를 통해 추가하고 고유 문자열 route를 설정
-                // 탐색 그래프에 대상이 추가되므로 이 대상으로 이동하면 표시될 UI도 정의해야함
-                composable(route = Overview.route) {
-                    OverviewScreen(
-                        onClickSeeAllAccounts = {
-                            navController.navigateSingleTopTo(Accounts.route)
-                        },
-                        onClickSeeAllBills = {
-                            navController.navigateSingleTopTo(Bills.route)
-                        },
-                        onAccountClick = { accountType ->
-                            navController.navigateToSingleAccount(accountType)
-                        }
-                    )
-                }
-                composable(route = Accounts.route) {
-                    AccountsScreen(
-                        onAccountClick = { accountType ->
-                            navController.navigateToSingleAccount(accountType)
-                        }
-                    )
-                }
-                composable(route = Bills.route) {
-                    BillsScreen()
-                }
-                composable(
-                    route = SingleAccount.routeWithArgs,
-                    // 경로에 파라미터를 포함
-                    arguments = SingleAccount.arguments,
-                    // composable은 arguments를 통해 인수를 처리하도록 함
-                    deepLinks = SingleAccount.deepLinks
-                    // deepLinks 매개변수 추가
-                    // intent-filter에 정의된 것과 일치하는 uriPattern을 전달한다. accountTypeArg 인수도 추가해야 한다.
-                ) { navBackStackEntry ->
-                    val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-                    SingleAccountScreen(accountType)
-                    // 전달된 인수 값 가져오기
-                    // 백 스택에 있는 항목의 현재 경로 및 전달된 인수에 관한 정보를 저장하는 클래스인 NavBackStackEntry에 액세스할 수 있다.
-                    // navBackStackEntry에서 arguments 목록을 가져온 뒤 필요한 인수를 검색하고 가져와서 컴포저블 화면으로 전달할 수 있다.
-                }
-            }
+            )
         }
     }
-}
-
-fun NavHostController.navigateSingleTopTo(route: String) = this.navigate(route) {
-    popUpTo(
-        this@navigateSingleTopTo.graph.findStartDestination().id
-    ) {
-        saveState = true
-    }
-    // 새로 탭을 눌렀을 때 이전 화면 스택들을 정리하고, 탭의 첫 화면만 남기는데 상태는 저장한다.
-    launchSingleTop = true
-    // 백 스택 위에 대상 탭이 최대 1개만 있도록
-    // 동일한 탭을 여러 번 탭해도 동일한 대상의 사본이 여러 개 실행되지 않는다.
-    // 새로운 인스턴스를 생성하지 않고 기존 것을 재활용한다.
-    restoreState = true
-    // 이전에 PopUpToBuilder.saveState 또는 popUpToSaveState 속성에 의한 저장된 상태 복원 여부 결정
-}
-
-private fun NavHostController.navigateToSingleAccount(accountType: String) {
-    this.navigateSingleTopTo("${SingleAccount.route}/$accountType")
 }
