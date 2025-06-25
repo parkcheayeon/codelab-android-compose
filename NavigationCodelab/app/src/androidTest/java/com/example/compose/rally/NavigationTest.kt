e5feb0a8267cd3def9159f868d491a1842eeb302
parkcheayeon <ddaany927@gmail.com>
2025-06-26 00:19:19 +0900

    @Before 함수 추가

--- NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
+++ NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
-import junit.framework.TestCase.fail
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,11 +14,10 @@ class NavigationTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-
     lateinit var navController: TestNavHostController
 
-    @Test
-    fun rallyNavHost_verifyOverviewStartDestination() {
+    @Before
+    fun setupRallyNavHost() {
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current)
             navController.navigatorProvider.addNavigator(
@@ -26,9 +25,10 @@ class NavigationTest {
             )
             RallyNavHost(navController = navController)
         }
-        //fail()
-        // 실제 어설션이 만들어질 때까지 테스트가 실패하도록 한다. 테스트 구현을 완료하라는 알림 역할을 한다.
+    }
 
+    @Test
+    fun rallyNavHost_verifyOverviewStartDestination() {
         composeTestRule
             .onNodeWithContentDescription("Overview Screen")
             .assertIsDisplayed()
