2ca1260c5270b2e16085b13e1c1525e89af402be
parkcheayeon <ddaany927@gmail.com>
2025-06-26 00:56:18 +0900

    주석 추가

--- NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
+++ NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -17,11 +17,15 @@ class NavigationTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+    // Compose 테스트 규칙
+    // Compose 테스트 API를 사용하고 Compose를 사용하여 컴포저블과 애플리케이션을 테스트 및 제어한다.
     lateinit var navController: TestNavHostController
+    // 테스트 전용 네비게이션 컨트롤러
 
     @Before
     fun setupRallyNavHost() {
         composeTestRule.setContent {
+            // 테스트하려는 Compose 콘텐츠 설정
             navController = TestNavHostController(LocalContext.current)
             navController.navigatorProvider.addNavigator(
                 ComposeNavigator()
@@ -29,6 +33,8 @@ class NavigationTest {
             RallyNavHost(navController = navController)
         }
     }
+    // 테스트하기 전에 먼저 계층 구조를 구성해야 한다.
+    // 테스트 어설션과 확인은 setContent 함수 외부에서 해당 함수 뒤에 작성해야 한다
 
     @Test
     fun rallyNavHost_verifyOverviewStartDestination() {
